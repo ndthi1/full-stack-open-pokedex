9b4577f982966f06f484ef608a19472b54a3f51e
DangThi <19127276@student.hcmus.edu.vn>
2022-03-19 21:26:08 +0700

    update eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,7 @@ module.exports = {
     "browser": true,
     "es6": true,
     "jest/globals": true,
-      'env': {
-        'commonjs': true,
-        'node': true
-    },
+    "node":true,
   },
   "extends": [
     "eslint:recommended",
